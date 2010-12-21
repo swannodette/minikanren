a43f4b5b775487b21c024c268898eb3c321d2584
David Nolen <david.nolen@gmail.com>
2010-12-20 22:56:21 -0500

    * miniKanren.rkt: the whole of miniKanren, not tested yet tho

--- miniKanren.rkt
+++ miniKanren.rkt
@@ -33,8 +33,9 @@
     (let ((v (walk v s)))
       (cond
        ((var? v) (eq? v x))
-       ((pair? v) (or (occurs-check x (car v) s)
-                      (occurs-check x (cdr v) s)))
+       ((pair? v)
+        (or (occurs-check x (car v) s)
+            (occurs-check x (cdr v) s)))
        (else #f)))))
 
 (define ext-s
@@ -50,10 +51,166 @@
       (cond
        ((eq? u v) s)
        ((var? u) (cond
-                  ((varl? v) (ext-s-no-check u v s))
+                  ((var? v) (ext-s-no-check u v s))
                   (else (ext-s u v s))))
        ((var? v) (ext-s v u s))
-       ((and (pair? u) (pair? v)) (let ((s (unify (cdr u) (cdr v))))
-                                    (and s (unify (cdr u) (cdr v) s))))
+       ((and (pair? u) (pair? v))
+        (let ((s (unify (cdr u) (cdr v) s)))
+          (and s (unify (cdr u) (cdr v) s))))
        ((equal? u v) s)
        (else #f)))))
+
+(define unify-no-check
+  (lambda (u v s)
+    (let ((u (walk u s))
+          (v (walk v s)))
+      (cond
+       ((eq? u v) s)
+       ((var? u) (ext-s-no-check u v s))
+       ((var? v) (ext-s-no-check v u s))
+       ((and (pair? u) (pair? v))
+        (let ((s (unify-no-check (car u) (car v) s)))
+          (and s (unify-no-check (cdr u) (cdr v) s))))
+       ((equal? u v) s)
+       (else #f)))))
+
+(define reify
+  (lambda (v s)
+    (let ((v (walk* v s)))
+      (walk* v (reify-s v empty-s)))))
+
+(define walk*
+  (lambda (v s)
+    (let ((v (walk v s)))
+      (cond
+       ((var? v) v)
+       ((pair? v) (cons (walk* (car v) s)
+                        (walk* (cdr v) s)))
+       (else v)))))
+
+(define reify-name
+  (lambda (n)
+    (string->symbol (string-append "_." (number->string n)))))
+
+(define reify-s
+  (lambda (v s)
+    (let ((v (walk v s)))
+      (cond
+       ((var? v) (ext-s (reify-name (length s) s)))
+       ((pair? v) (reify-s (cdr v) (reify-s (car v) s)))
+       (else s)))))
+
+(define-syntax lambdag@
+  (syntax-rules ()
+    ((_ (s) e) (lambda (s) e))))
+
+(define-syntax lambdaf@
+  (syntax-rules ()
+    ((_ () e) (lambda () e))))
+
+(define-syntax mzero
+  (syntax-rules ()
+    ((_) #f)))
+
+(define-syntax unit
+  (syntax-rules ()
+    ((_ a) a)))
+
+(define-syntax choice
+  (syntax-rules ()
+    ((_ a f) (cons a f))))
+
+(define-syntax inc
+  (syntax-rules ()
+    ((_ e) (lambdaf@ () e))))
+
+(define-syntax case-inf
+  (syntax-rules ()
+    ((_ e (() e0) ((fp) e1) ((ap) e2) ((a f) e3))
+     (let ((a-inf e))
+       (cond
+        ((not a-inf) e0)
+        ((procedure? a-inf) (let ((fp a-inf))
+                              e1))
+        ((and (pair? a-inf) (procedure? (cdr a-inf)))
+         (let ((a (car a-inf)) (f (cdr a-inf))) e3))
+        (else (let ((ap a-inf)) e2)))))))
+
+(define-syntax ==
+  (syntax-rules ()
+    ((_ u v)
+     (lambdag@ (a)
+       (cond
+        ((unify u v a) => (lambda (a) (unit a)))
+        (else (mzero)))))))
+
+(define-syntax ==-no-check
+  (syntax-rules ()
+    ((_ u v)
+     (lambdag@ (a)
+               (cond
+                ((unify-no-check u v a) => (lambda (a) (unit a)))
+                (else (mzero)))))))
+
+(define-syntax conde
+  (syntax-rules ()
+    ((_ (g0 g ...) (g1 gp ...) ...)
+     (lambdag@ (a)
+               (inc
+                (mplus* (bind* (g0 a) g ...)
+                        (bind* (g1 a) gp ...)
+                        ...))))))
+
+(define-syntax mplus*
+  (syntax-rules ()
+    ((_ e) e)
+    ((_ e0 e ...) (mplus e0 (lambdaf@ () (mplus* e ...))))))
+
+(define mplus
+  (lambda (a-inf f)
+    (case-inf a-inf
+              (() (f))
+              ((fp) (inc (mplus (f) fp)))
+              ((a) (choice a f))
+              ((a fp) (choice a (lambdaf@ () (mplus (f) fp)))))))
+
+(define-syntax exist
+  (syntax-rules ()
+    ((_ (x ...) g0 g ...)
+     (lambdag@ (a)
+               (inc
+                (let ((x (var x)) ...)
+                  (bind* (g0 a) g ...)))))))
+
+(define-syntax bind*
+  (syntax-rules ()
+    ((_ e) e)
+    ((_ e g0 g ...) (bind* (bind e g0) g ...))))
+
+(define bind
+  (lambda (a-inf g)
+    (case-inf a-inf
+              (() (mzero))
+              ((f) (inc (bind (f) g)))
+              ((a) (g a))
+              ((a f) (mplus (g a) (lambdaf@ () (bind (f) g)))))))
+
+(define-syntax run
+  (syntax-rules ()
+    ((_ n (x) g0 g ...)
+     (take n
+           (lambdaf@ ()
+                     ((exist (x) g0 g ...
+                             (lambdag@ (a)
+                                       (cons (reify x a) '())))
+                      empty-s))))))
+
+(define take
+  (lambda (n f)
+    (if (and n (zero? n))
+        '()
+        (case-inf (f)
+                  (() '())
+                  ((f) (take n f))
+                  ((a) a)
+                  ((a f) (cons (car a) (take (and n (- n 1)) f)))))))
