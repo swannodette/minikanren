9e50e956d64be5bac27af004f192a35bc6c834cd
David Nolen <david.nolen@gmail.com>
2011-02-20 15:09:04 -0600

    * miniKanren.rkt: formatting, fixed bug in walk

--- miniKanren.rkt
+++ miniKanren.rkt
@@ -20,59 +20,60 @@
 
 (define walk
   (lambda (v s)
+    (display (list v s))
     (cond
-     ((var? v)
-      (let ((a (assq v s)))
-        (cond
-         ((a (walk (rhs a))))
-         (else v))))
-     (else v))))
+      ((var? v)
+       (let ((a (assq v s)))
+         (cond
+           (a (walk (rhs a)))
+           (else v))))
+      (else v))))
 
 (define occurs-check
   (lambda (x v s)
     (let ((v (walk v s)))
       (cond
-       ((var? v) (eq? v x))
-       ((pair? v)
-        (or (occurs-check x (car v) s)
-            (occurs-check x (cdr v) s)))
-       (else #f)))))
+        ((var? v) (eq? v x))
+        ((pair? v)
+         (or (occurs-check x (car v) s)
+             (occurs-check x (cdr v) s)))
+        (else #f)))))
 
 (define ext-s
   (lambda (x v s)
     (cond
-     ((occurs-check x v s) #f)
-     (else (ext-s-no-check x v s)))))
+      ((occurs-check x v s) #f)
+      (else (ext-s-no-check x v s)))))
 
 (define unify
   (lambda (u v s)
     (let ((u (walk u s))
           (v (walk v s)))
       (cond
-       ((eq? u v) s)
-       ((var? u) (cond
-                  ((var? v) (ext-s-no-check u v s))
-                  (else (ext-s u v s))))
-       ((var? v) (ext-s v u s))
-       ((and (pair? u) (pair? v))
-        (let ((s (unify (cdr u) (cdr v) s)))
-          (and s (unify (cdr u) (cdr v) s))))
-       ((equal? u v) s)
-       (else #f)))))
+        ((eq? u v) s)
+        ((var? u) (cond
+                    ((var? v) (ext-s-no-check u v s))
+                    (else (ext-s u v s))))
+        ((var? v) (ext-s v u s))
+        ((and (pair? u) (pair? v))
+         (let ((s (unify (cdr u) (cdr v) s)))
+           (and s (unify (cdr u) (cdr v) s))))
+        ((equal? u v) s)
+        (else #f)))))
 
 (define unify-no-check
   (lambda (u v s)
     (let ((u (walk u s))
           (v (walk v s)))
       (cond
-       ((eq? u v) s)
-       ((var? u) (ext-s-no-check u v s))
-       ((var? v) (ext-s-no-check v u s))
-       ((and (pair? u) (pair? v))
-        (let ((s (unify-no-check (car u) (car v) s)))
-          (and s (unify-no-check (cdr u) (cdr v) s))))
-       ((equal? u v) s)
-       (else #f)))))
+        ((eq? u v) s)
+        ((var? u) (ext-s-no-check u v s))
+        ((var? v) (ext-s-no-check v u s))
+        ((and (pair? u) (pair? v))
+         (let ((s (unify-no-check (car u) (car v) s)))
+           (and s (unify-no-check (cdr u) (cdr v) s))))
+        ((equal? u v) s)
+        (else #f)))))
 
 (define reify
   (lambda (v s)
@@ -83,10 +84,10 @@
   (lambda (v s)
     (let ((v (walk v s)))
       (cond
-       ((var? v) v)
-       ((pair? v) (cons (walk* (car v) s)
-                        (walk* (cdr v) s)))
-       (else v)))))
+        ((var? v) v)
+        ((pair? v) (cons (walk* (car v) s)
+                         (walk* (cdr v) s)))
+        (else v)))))
 
 (define reify-name
   (lambda (n)
@@ -96,9 +97,9 @@
   (lambda (v s)
     (let ((v (walk v s)))
       (cond
-       ((var? v) (ext-s (reify-name (length s) s)))
-       ((pair? v) (reify-s (cdr v) (reify-s (car v) s)))
-       (else s)))))
+        ((var? v) (ext-s (reify-name (length s) s)))
+        ((pair? v) (reify-s (cdr v) (reify-s (car v) s)))
+        (else s)))))
 
 (define-syntax lambdag@
   (syntax-rules ()
@@ -129,28 +130,28 @@
     ((_ e (() e0) ((fp) e1) ((ap) e2) ((a f) e3))
      (let ((a-inf e))
        (cond
-        ((not a-inf) e0)
-        ((procedure? a-inf) (let ((fp a-inf))
-                              e1))
-        ((and (pair? a-inf) (procedure? (cdr a-inf)))
-         (let ((a (car a-inf)) (f (cdr a-inf))) e3))
-        (else (let ((ap a-inf)) e2)))))))
+         ((not a-inf) e0)
+         ((procedure? a-inf) (let ((fp a-inf))
+                               e1))
+         ((and (pair? a-inf) (procedure? (cdr a-inf)))
+          (let ((a (car a-inf)) (f (cdr a-inf))) e3))
+         (else (let ((ap a-inf)) e2)))))))
 
 (define-syntax ==
   (syntax-rules ()
     ((_ u v)
      (lambdag@ (a)
-       (cond
-        ((unify u v a) => (lambda (a) (unit a)))
-        (else (mzero)))))))
+               (cond
+                 ((unify u v a) => (lambda (a) (unit a)))
+                 (else (mzero)))))))
 
 (define-syntax ==-no-check
   (syntax-rules ()
     ((_ u v)
      (lambdag@ (a)
                (cond
-                ((unify-no-check u v a) => (lambda (a) (unit a)))
-                (else (mzero)))))))
+                 ((unify-no-check u v a) => (lambda (a) (unit a)))
+                 (else (mzero)))))))
 
 (define-syntax conde
   (syntax-rules ()
@@ -179,7 +180,7 @@
     ((_ (x ...) g0 g ...)
      (lambdag@ (a)
                (inc
-                (let ((x (var x)) ...)
+                (let ((x (var 'x)) ...)
                   (bind* (g0 a) g ...)))))))
 
 (define-syntax bind*
