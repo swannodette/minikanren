44311c9768c219538cdd32e4cbf02f4b83c82a1b
David Nolen <david.nolen@gmail.com>
2011-03-05 14:05:37 -0500

    * order.rkt: illustration that miniKanren interleaving is not that fair when there are many recursive clauses in a conde

--- order.rkt
+++ order.rkt
@@ -8,23 +8,35 @@
 
 (define repeat
   (lambda (n)
-    (lambda (x)
-      (conde
-       ((== x n))
-       ((ones x))))))
+    (define repeater
+      (lambda (x)
+        (conde
+         ((== x n))
+         ((repeater x)))))
+    repeater))
 
 (define ones (repeat 1))
 (define twos (repeat 2))
 (define threes (repeat 3))
+(define fours (repeat 4))
 
 (define test
   (lambda (x)
     (conde
-     (ones)
-     (twos)
-     (threes))))
+     ((ones x))
+     ((twos x))
+     ((threes x))
+     ((fours x)))))
 
 (comment
  (run 4 [q] (ones q))
  (run 4 [q] (twos q))
+ (run 4 [q] (threes q))
+
+ ;; miniKanren's interleaving strategy is not great
+ ;; it suffers from the same problem that mplus-concat had
+ ;; when dealing with many recursive goals generators
+ ;; goals further down the conde don't actually get their
+ ;; results interleaved fairly
+ (run 16 [q] (test q))
  )
