152bf182d909ea2d61387bcdd122f1bf7d362466
David Nolen <david.nolen@gmail.com>
2011-03-08 01:28:23 -0500

    * matche.rkt: lindsey kuper's matche, fixed a bug in her code - lambdae was borked.

--- matche.rkt
+++ matche.rkt
@@ -2,61 +2,70 @@
 
 (require "minikanren.rkt")
 
-(provide lambdae matche)
+(provide lambdae
+         matche
+         exist*)
+
+;; Example:
+;; (print-gensym #f)
+;; (pretty-print (expand '(matche '(1 2 3) ((,a ,b ,c) 1) ((,x d ,y) 2) (5) (,w) ((a b c)))))
+;; (print-gensym #t)
 
 (define-syntax lambdae
   (syntax-rules ()
     ((_ (x ...) c c* ...)
-     (lambda (x ...) (matche (quasiqoute (unquote x) ...) (c c* ...) ())))))
+     (lambda (x ...) (matche (list x ...) c c* ...)))))
 
-(define-syntax exist*
+(define-syntax exist* ;;; easy way to deal with duplicate vars (as if exist used let*)
   (syntax-rules ()
-    ((_ (x ...) g0 g ...)
-     (lambdag@ (a)
-       (inc
-         (let* ((x (var 'x)) ...)
-           (bind* (g0 a) g ...)))))))
-
-(define-syntax fresh*
-  (syntax-rules ()
-    ((_ (x ...) g0 g ...)
-     (lambdag@ (a)
-       (inc
-         (let* ((x (nom 'x)) ...)
-           (bind* (g0 a) g ...)))))))
+    ((_ () g ...) (exist () g ...))  ;;; just in case there were no vars in pattern.
+    ((_ (x) g ...) (exist (x) g ...)) ;;; exactly one var
+    ((_ (y x z ...) g ...) (exist (y) (exist* (x z ...) g ...))))) ;;; more than one.
 
 (define-syntax matche
   (syntax-rules ()
-    ((_ (f x ...) g* . cs)
-     (let ((v (f x ...))) (matche v g* . cs)))
-    ((_ v g* . cs) (mpat conde v (g* . cs) ()))))
 
-(define-syntax mpat
-  (syntax-rules (_ quote unquote unquote-splicing expand cons)
-    ((_ co v () (l ...))  (co l ...))
-    ((_ co v (pat) xs as ((g ...) . cs) (l ...))
-     (mpat co v cs (l ... ((fresh* as (exist* xs (== pat v) g ...))))))
-    ((_ co v ((_ g0 g ...) . cs) (l ...))
-     (mpat co v cs (l ... ((exist () g0 g ...)))))
-    ((_ co v (((unquote y) g0 g ...) . cs) (l ...))
-     (mpat co v cs (l ... ((exist (y) (== y v) g0 g ...)))))
-    ((_ co v (((unquote-splicing b) g0 g ...) . cs) (l ...))
-     (mpat co v cs (l ... ((fresh (b) g0 g ...)))))
-    ((_ co v ((pat g ...) . cs) ls)
-     (mpat co v (pat expand) () () ((g ...) . cs) ls))
-    ((_ co (_ expand . k) (x ...) as cs ls)
-     (mpat co v ((unquote y) . k) (y x ...) as cs ls))
-    ((_ co v ((unquote y) expand . k) (x ...) as cs ls)
-     (mpat co v ((unquote y) . k) (y x ...) as cs ls))
-    ((_ co v ((unquote-splicing b) expand . k) xs (a ...) cs ls) 
-     (mpat co v ((unquote b) . k) xs (b a ...) cs ls))
-    ((_ co v ((quote c) expand . k) xs as cs ls)
-     (mpat co v (c . k) xs as cs ls))
-    ((_ co v ((a . d) expand . k) xs as cs ls)
-     (mpat co v (d expand a expand cons . k) xs as cs ls))
-    ((_ co v (d a expand cons . k) xs as cs ls)
-     (mpat co v (a expand d cons . k) xs as cs ls))
-    ((_ co v (a d cons . k) xs as cs ls)
-     (mpat co v ((a . d) . k) xs as cs ls))
-    ((_ co v (c expand . k) xs as cs ls)
-     (mpat co v (c . k) xs as cs ls))))
+    ((_ (f x ...) g* . cs)
+     (let ((v (f x ...))) ;;; evaluate first argument once.
+       (matche v g* . cs))) 
+
+    ((_ v (pat g ...) ...) ;;; pass to driver list of uns and (empty) list of dones.
+     (mpat0 ((pat (exist* () (== `pat v) g ...)) ...) ()))))
+
+(define-syntax mpat0  ;;; body is alwasys (exist* (x ...) g ...)
+  (syntax-rules ()
+    
+    ((_ () (done ...)) (conde done ...))  ;;; all done (no more undone)
+    
+    ((_ ((pat body) un* ...) done*) (mpat pat () body (un* ...) done*)))) ;;; do one un.
+
+(define-syntax mpat ;;; virtually the same reasoning as earlier versions.
+  (syntax-rules (unquote exist*)
+
+    ((_ (unquote x) () (exist* (y ...) g ...) un* (done ...))
+     (mpat0 un* (done ... ((exist* (y ... x) g ...))))) ;;; empty stack. add var; one un done.
+                                                        ;;; turn g to clause.
+    
+    ((_ (unquote x) (top stack ...) (exist* (y ...) g ...) un* done*) 
+     (mpat top (stack ...) (exist* (y ... x) g ...) un* done*)) ;;; pop; add var
+    
+    ((_ (a . d) (top ...) body un* done*) (mpat a (d top ...) body un* done*)) ;;; push d
+    
+    ((_ ignore () body un* (done ...)) (mpat0 un* (done ... (body)))) ;;; empty stack
+                                                                      ;;; turn g to clause.
+    
+    ((_ ignore (top stack ...) body un* done*) (mpat top (stack ...) body un* done*)))) ; pop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
